1244d558491cf9dc88df98411f353d05e861cdc4
Jack Boylan <boylan.jack@gmail.com>
2024-10-08 09:58:03 +0000

    Updates docred eval script

--- data/docred/docred_evaluation.py
+++ data/docred/docred_evaluation.py
@@ -20,7 +20,7 @@ def load_model(checkpoint_dir):
 
 def run_inference(test_set, model):
     model.base_config.fixed_relation_types = False
-    results, micro_f1, macro_f1, preds = model.evaluate(
+    results, metric_dict, preds = model.evaluate(
         test_set, 
         flat_ner=True, 
         threshold=0.000001, 
@@ -29,6 +29,9 @@ def run_inference(test_set, model):
         top_k=1,
         return_preds=True
     )
+    micro_f1, micro_precision, micro_recall = metric_dict['micro_f1'], metric_dict['micro_precision'], metric_dict['micro_recall']
+    macro_f1, macro_precision, macro_recall = metric_dict['macro_f1'], metric_dict['macro_precision'], metric_dict['macro_recall']
+
     return preds
 
 if __name__ == "__main__":
