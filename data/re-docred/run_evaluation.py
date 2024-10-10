c5f71c65d9d7e5ae2e4a34d3682a1c0913a4dc7d
Jack Boylan <boylan.jack@gmail.com>
2024-10-10 08:58:35 +0000

    Runs redocred evaluation for both gold and pred coref clusters at once

--- data/re-docred/run_evaluation.py
+++ data/re-docred/run_evaluation.py
@@ -57,7 +57,7 @@ def run_inference(test_set, model):
         test_set, 
         flat_ner=True, 
         threshold=model.base_config.eval_threshold, 
-        batch_size=16,
+        batch_size=8,
         relation_types=[],
         top_k=1,
         return_preds=True
@@ -90,7 +90,7 @@ def run_evaluation(ckpt_dir, use_gold_coref=False, use_auxiliary_coref=False, mo
     # Check if ckpt_dir exists and redirect print statements to a file if it does
     if os.path.exists(ckpt_dir):
         log_file = os.path.join(ckpt_dir, 'evaluation_log.txt')
-        sys.stdout = open(log_file, 'w')
+        sys.stdout = open(log_file, 'a+')
 
 
     # Load the test set
@@ -123,129 +123,135 @@ def run_evaluation(ckpt_dir, use_gold_coref=False, use_auxiliary_coref=False, mo
     print(f"Inference done! Results saved to {INTERMEDIATE_RESULTS_PATH}")
 
     # get coreference clusters
+    coref_clusters = []
     if use_auxiliary_coref: 
         raise NotImplementedError("Not implemented yet!")
         # TODO run fast-coref or some other coref model
     elif use_gold_coref: 
-        clusters_batch, entity_to_cluster_idx = get_gold_coreference_clusters(test_set)
-    else:
-        # use predicted coreference clusters
-        print("Using predicted coreference clusters...")
-        clusters_batch, entity_to_cluster_idx = utils.get_coreference_clusters(preds)
+        gold_coref_clusters_batch, gold_coref_entity_to_cluster_idx = get_gold_coreference_clusters(test_set)
+        coref_clusters.append((gold_coref_clusters_batch, gold_coref_entity_to_cluster_idx, 'GOLD_COREF'))
+
+    # predicted coreference clusters
+    pred_coref_clusters_batch, pred_coref_entity_to_cluster_idx = utils.get_coreference_clusters(preds)
+    coref_clusters.append((pred_coref_clusters_batch, pred_coref_entity_to_cluster_idx, 'PRED_COREF'))
 
     # entity_to_cluster_idx_list --> (128, 129): 0, (33, 34): 0, (144, 145): 0, (0, 6): 0, ...
 
 
-    # propagate labels using coreference clusters
-    cluster_relations_list = utils.aggregate_cluster_relations(entity_to_cluster_idx, preds)
-
-    
-    # [ [{'h_idx': 0, 't_idx': 2, 'r': 'performer'}, ...], ...]
-
-    submission = []
-    for example, cluster_relations in zip(test_set, cluster_relations_list):
-        for rel in cluster_relations:
-            instance = {}
-            instance['title'] = example['title']
-            instance['h_idx'] = rel['h_idx']
-            instance['t_idx'] = rel['t_idx']
-            instance['r'] = rel2id.get(rel['r'], 'NA')
-            instance['evidence'] = None
-            submission.append(instance)
-
-
-    '''
-    submission: [
-    {"title": "Loud Tour", "h_idx": 0, "t_idx": 2, "r": "P175", "evidence": null}, 
-    {"title": "Loud Tour", "h_idx": 0, "t_idx": 2, "r": "P175", "evidence": null}, 
-    {"title": "Loud Tour", "h_idx": 0, "t_idx": 2, "r": "P175", "evidence": null},
-    ... 
-    '''
+    for (clusters_batch, entity_to_cluster_idx, coref_type) in coref_clusters:
         
-    # save submission file
-    with open(SUBMISSION_PATH, 'w') as f:
-        json.dump(submission, f)
-    print(f"Submission file saved to {SUBMISSION_PATH}!")
+        print(f"Evaluating using {coref_type} coreference clusters...")
+        
+        # propagate labels using coreference clusters
+        cluster_relations_list = utils.aggregate_cluster_relations(entity_to_cluster_idx, preds)
 
-    with open(SUBMISSION_PATH, 'r') as f:
-        submission = json.load(f)
+        
+        # [ [{'h_idx': 0, 't_idx': 2, 'r': 'performer'}, ...], ...]
 
-    # run docred eval script
-    print("Running evaluation script...")
-    best_f1, _, best_f1_ign, _, best_p, best_r, debug_results = official_evaluate(   # official_evaluate_benchmark
-        tmp=submission, 
-        path='data/re-docred', 
-        train_file='data/train_revised.json', 
-        dev_file='data/test_revised.json', 
-    )
-    print(f"Scores: F1: {best_f1}, F1 Ignore: {best_f1_ign} Precision: {best_p}, Recall: {best_r}")
+        submission = []
+        for example, cluster_relations in zip(test_set, cluster_relations_list):
+            for rel in cluster_relations:
+                instance = {}
+                instance['title'] = example['title']
+                instance['h_idx'] = rel['h_idx']
+                instance['t_idx'] = rel['t_idx']
+                instance['r'] = rel2id.get(rel['r'], 'NA')
+                instance['evidence'] = None
+                submission.append(instance)
 
 
-    # Create a mapping from title to index
-    title_to_index = {example['title']: idx for idx, example in enumerate(test_set)}
-    batch_tokenized_text = [example['tokenized_text'] for example in test_set]
+        '''
+        submission: [
+        {"title": "Loud Tour", "h_idx": 0, "t_idx": 2, "r": "P175", "evidence": null}, 
+        {"title": "Loud Tour", "h_idx": 0, "t_idx": 2, "r": "P175", "evidence": null}, 
+        {"title": "Loud Tour", "h_idx": 0, "t_idx": 2, "r": "P175", "evidence": null},
+        ... 
+        '''
+            
+        # save submission file
+        with open(SUBMISSION_PATH, 'w') as f:
+            json.dump(submission, f)
+        print(f"Submission file saved to {SUBMISSION_PATH}!")
 
-    # Process false positives
-    print("\nProcessing false positives:")
-    for x in debug_results['false_positives']:
-        title, r, h_idx, t_idx = x['title'], x['r'], x['h_idx'], x['t_idx']
+        with open(SUBMISSION_PATH, 'r') as f:
+            submission = json.load(f)
 
-        idx = title_to_index.get(title)
-        if idx is None:
-            print(f"Title not found: {title}")
-            continue
-        clusters = clusters_batch[idx]
-        tokenized_text = batch_tokenized_text[idx]
-        if h_idx >= len(clusters) or t_idx >= len(clusters):
-            print(f"Invalid h_idx or t_idx for title {title}")
-            continue
-        head_cluster_positions = clusters[h_idx]
-        tail_cluster_positions = clusters[t_idx]
-        head_cluster = [' '.join(tokenized_text[s:e]) for s, e in head_cluster_positions]
-        tail_cluster = [' '.join(tokenized_text[s:e]) for s, e in tail_cluster_positions]
-        pred_relation = id2rel.get(r, 'NA')
-        if (title, h_idx, t_idx) in debug_results['actual']: 
-            # there is an actual relation that was misclassified
-            actual_relation = debug_results['actual'][(title, h_idx, t_idx)]
-            actual_relation = id2rel[actual_relation]
-        else:
-            # there's no relation between head and tail
-            actual_relation = 'NA'  
-        print(f"##################### False Positive in: {title}\n")
-        print(f"Predicted Relation: {pred_relation}")
-        print(f"Actual Relation: {actual_relation}")
-        print(f"Head Cluster: {head_cluster}")
-        print(f"Tail Cluster: {tail_cluster}")
+        # run docred eval script
+        print("Running official_evaluate script...")
+        best_f1, _, best_f1_ign, _, best_p, best_r, debug_results = official_evaluate(   # official_evaluate_benchmark
+            tmp=submission, 
+            path='data/re-docred', 
+            train_file='data/train_revised.json', 
+            dev_file='data/test_revised.json', 
+        )
+        print(f"Scores: F1: {best_f1}, F1 Ignore: {best_f1_ign} Precision: {best_p}, Recall: {best_r}")
 
-    # Process false negatives
-    print("\nProcessing false negatives:")
-    for title, r, h_idx, t_idx in debug_results['false_negatives']:
 
-        idx = title_to_index.get(title)
-        if idx is None:
-            print(f"Title not found: {title}")
-            continue
-        clusters = clusters_batch[idx]
-        tokenized_text = batch_tokenized_text[idx]
-        if h_idx >= len(clusters) or t_idx >= len(clusters):
-            print(f"Invalid h_idx or t_idx for title {title}")
-            continue
-        head_cluster_positions = clusters[h_idx]
-        tail_cluster_positions = clusters[t_idx]
-        head_cluster = [' '.join(tokenized_text[s:e]) for s, e in head_cluster_positions]
-        tail_cluster = [' '.join(tokenized_text[s:e]) for s, e in tail_cluster_positions]
-        actual_relation = id2rel.get(r, 'NA')
-        print(f"##################### False Negative in: {title}\n")
-        print(f"Actual Relation: {actual_relation}")
-        print(f"Head Cluster: {head_cluster}")
-        print(f"Tail Cluster: {tail_cluster}")
+        # Create a mapping from title to index
+        title_to_index = {example['title']: idx for idx, example in enumerate(test_set)}
+        batch_tokenized_text = [example['tokenized_text'] for example in test_set]
+
+        # Process false positives
+        print("\nProcessing false positives:")
+        for x in debug_results['false_positives']:
+            title, r, h_idx, t_idx = x['title'], x['r'], x['h_idx'], x['t_idx']
+
+            idx = title_to_index.get(title)
+            if idx is None:
+                print(f"Title not found: {title}")
+                continue
+            clusters = clusters_batch[idx]
+            tokenized_text = batch_tokenized_text[idx]
+            if h_idx >= len(clusters) or t_idx >= len(clusters):
+                print(f"Invalid h_idx or t_idx for title {title}")
+                continue
+            head_cluster_positions = clusters[h_idx]
+            tail_cluster_positions = clusters[t_idx]
+            head_cluster = [' '.join(tokenized_text[s:e]) for s, e in head_cluster_positions]
+            tail_cluster = [' '.join(tokenized_text[s:e]) for s, e in tail_cluster_positions]
+            pred_relation = id2rel.get(r, 'NA')
+            if (title, h_idx, t_idx) in debug_results['actual']: 
+                # there is an actual relation that was misclassified
+                actual_relation = debug_results['actual'][(title, h_idx, t_idx)]
+                actual_relation = id2rel[actual_relation]
+            else:
+                # there's no relation between head and tail
+                actual_relation = 'NA'  
+            print(f"##################### False Positive in: {title}\n")
+            print(f"Predicted Relation: {pred_relation}")
+            print(f"Actual Relation: {actual_relation}")
+            print(f"Head Cluster: {head_cluster}")
+            print(f"Tail Cluster: {tail_cluster}")
+
+        # Process false negatives
+        print("\nProcessing false negatives:")
+        for title, r, h_idx, t_idx in debug_results['false_negatives']:
+
+            idx = title_to_index.get(title)
+            if idx is None:
+                print(f"Title not found: {title}")
+                continue
+            clusters = clusters_batch[idx]
+            tokenized_text = batch_tokenized_text[idx]
+            if h_idx >= len(clusters) or t_idx >= len(clusters):
+                print(f"Invalid h_idx or t_idx for title {title}")
+                continue
+            head_cluster_positions = clusters[h_idx]
+            tail_cluster_positions = clusters[t_idx]
+            head_cluster = [' '.join(tokenized_text[s:e]) for s, e in head_cluster_positions]
+            tail_cluster = [' '.join(tokenized_text[s:e]) for s, e in tail_cluster_positions]
+            actual_relation = id2rel.get(r, 'NA')
+            print(f"##################### False Negative in: {title}\n")
+            print(f"Actual Relation: {actual_relation}")
+            print(f"Head Cluster: {head_cluster}")
+            print(f"Tail Cluster: {tail_cluster}")
+
+
+        print(f"Scores {coref_type}: F1: {best_f1}, F1 Ignore: {best_f1_ign} Precision: {best_p}, Recall: {best_r}")
 
     if log_file:
         sys.stdout.close()
         sys.stdout = sys.__stdout__
-
-    print(f"Scores: F1: {best_f1}, F1 Ignore: {best_f1_ign} Precision: {best_p}, Recall: {best_r}")
-
     return best_f1, best_f1_ign, best_p, best_r
 
 if __name__ == '__main__':
