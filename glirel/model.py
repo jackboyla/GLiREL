6ee3221a38329320d7a98714c90aa86da9111c58
q-oisinduffy <oisinduffy@quantexa.com>
2025-03-27 17:42:23 +0000

    updated test

--- glirel/model.py
+++ glirel/model.py
@@ -258,6 +258,7 @@ class GLiREL(InstructBase, PyTorchModelHubMixin):
         scores = self.scorer(rel_rep, rel_type_rep) # ([B, num_pairs, num_classes])
         bias = self.threshold_bias(rel_type_rep)
         scores = scores - bias
+        # update based on biased score
 
         return scores, num_classes, rel_type_mask, coref_scores  # ([B, num_pairs, num_classes]), num_classes, ([B, num_classes]), ([B, num_pairs, 1])
 
