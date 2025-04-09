98efb736ce69bc0e94580d2b6ce70233a7f29bb2
Jack Boylan <boylan.jack@gmail.com>
2025-04-09 09:33:54 +0100

    Updates quickstart to remove spacy and add `ground_truth_relations`

--- quickstart.py
+++ quickstart.py
@@ -1,18 +1,26 @@
 from glirel import GLiREL
-import spacy
 
 model = GLiREL.from_pretrained("jackboyla/glirel-large-v0")
-nlp = spacy.load('en_core_web_sm')
 
-text = 'Derren Nesbitt had a history of being cast in "Doctor Who", having played villainous warlord Tegana in the 1964 First Doctor serial "Marco Polo".'
-doc = nlp(text)
-tokens = [token.text for token in doc]
+tokens = ["The", "race", "took", "place", "between", "Godstow", "and", "Binsey", "along", "the", "Upper", "River", "Thames", "."]
 
-labels = ['country of origin', 'licensed to broadcast to', 'father', 'followed by', 'characters']
+labels = ['country of origin', 'located in or next to body of water', 'licensed to broadcast to', 'father', 'followed by', 'characters', ]
 
-ner = [[26, 27, 'PERSON', 'Marco Polo'], [22, 23, 'Q2989412', 'First Doctor']] # 'type' is not used -- it can be any string!
+ner = [
+    [7, 7, "Q4914513", "Binsey"], 
+    [11, 12, "Q19686", "River Thames"]
+]
 
-relations = model.predict_relations(tokens, labels, threshold=0.0, ner=ner, top_k=1)
+ground_truth_relations = [
+    {
+      "head": {"mention": "Binsey", "position": [7, 7], "type": "LOC"}, # 'type' is not used -- it can be any string!
+      "tail": {"mention": "River Thames", "position": [11, 12], "type": "Q19686"}, 
+      "relation_text": "located in or next to body of water"
+    }
+]
+
+relations, loss = model.predict_relations(tokens, labels, threshold=0.0, ner=ner, top_k=-1, ground_truth_relations=ground_truth_relations)
+# relations = model.predict_relations(tokens, labels, threshold=0.0, ner=ner, top_k=-1)
 
 print('Number of relations:', len(relations))
 
